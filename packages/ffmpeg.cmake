2e3d371e65fe365de5b92b9bdaf08e7946d8656f
FrozenRamune <66063808+FrozenRamune@users.noreply.github.com>
2025-05-24 07:40:48 +0000

    ffmpeg patch

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -33,9 +33,11 @@ ExternalProject_Add(ffmpeg
         dav1d
         openal-soft
     GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
+    GIT_TAG release/7.1
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
+    PATCH_COMMAND ${EXEC} git apply ffmpeg-dash.patch
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
         --cross-prefix=${TARGET_ARCH}-
