1fb33c6db11cbf31ec3dd20ad1feae8ccb2808c0
FrozenRamune <66063808+FrozenRamune@users.noreply.github.com>
2025-05-25 11:08:33 +0900

    Update ffmpeg.cmake

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -37,7 +37,7 @@ ExternalProject_Add(ffmpeg
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
-    PATCH_COMMAND ${EXEC} git apply ffmpeg-dash.patch
+    PATCH_COMMAND ${EXEC} git apply ${CMAKE_CURRENT_SOURCE_DIR}/ffmpeg-dash.patch
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
         --cross-prefix=${TARGET_ARCH}-
